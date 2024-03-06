35eb1ae8be8b41cd3495d89d414372e86d8c6651
Akmargam <aruway333@gmail.com>
2024-03-06 17:07:35 -0500

    switch jdk 12< with char

--- src/Student.java
+++ src/Student.java
@@ -6,26 +6,18 @@ public class Student {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Please enter the grade of the student in A,B,C and D ");
         char Input=(char) br.read();
-        String studentGrade=switch (Input){
-            case 'A'->"Excellent";
-            case 'B'->"Well Done";
-            case 'C'->"Just Passed";
-            case 'D'->"Take exam and Try again";
-            default -> "Sorry! No result";
-        };
-        System.out.println("Grade of the student is: "+Input +"\t"+" and can say: "+studentGrade);
-//        if(Input==1){
-//            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Apple");
-//        }
-//        else if (Input==2){
-//            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Samsung");
-//        }
-//        else if (Input==3){
-//            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Xiomi");
-//        }
-//        else if (Input==4){
-//            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Google Pixel");
-//        }
-//        else System.out.println("Please choose option from 1-4");
+        if(Input=='A'){
+            System.out.println("Grade of the student is: "+Input +"\t"+" and can say Excellent ");
+        }
+        else if (Input=='B'){
+            System.out.println("Grade of the student is: "+Input +"\t"+" and can say Well Done ");
+        }
+        else if (Input=='C'){
+            System.out.println("Grade of the student is: "+Input +"\t"+" and can say Just Passed ");
+        }
+        else if (Input=='D'){
+            System.out.println("Grade of the student is: "+Input +"\t"+" and can say Take exam and Try again");
+        }
+        else System.out.println("Please choose from option A,B,C and D");
     }
 }
