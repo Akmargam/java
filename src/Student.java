59c36fccdbb1f1953f9617673c94ba9058432328
Akmargam <aruway333@gmail.com>
2024-03-06 16:49:17 -0500

    switch jdk 12< with char

--- src/Student.java
+++ src/Student.java
@@ -4,32 +4,28 @@ import java.io.InputStreamReader;
 public class Student {
     public static void main(String[]args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please choose a number from 1 to 4: ");
-        int Input=Integer.parseInt(br.readLine());
-        /*String mobileName=switch (Input){
-            case 1->"Apple";
-            case 2->"Samsung";
-            case 3->{
-                System.out.println("There are a few Chinese mobile, however");
-                System.out.println("They are not good, nevertheless");
-                yield "Xiomi";
-            }
-            case 4->"Google Pixel";
-            default -> "Please choose option from 1-4";
+        System.out.println("Please enter the grade of the student in A,B,C and D ");
+        char Input=(char) br.read();
+        String studentGrade=switch (Input){
+            case 'A'->"Excellent";
+            case 'B'->"Well Done";
+            case 'C'->"Just Passed";
+            case 'D'->"Take exam and Try again";
+            default -> "Sorry! No result";
         };
-        System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: "+mobileName);*/
-        if(Input==1){
-            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Apple");
-        }
-        else if (Input==2){
-            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Samsung");
-        }
-        else if (Input==3){
-            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Xiomi");
-        }
-        else if (Input==4){
-            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Google Pixel");
-        }
-        else System.out.println("Please choose option from 1-4");
+        System.out.println("Grade of the student is: "+Input +"\t"+" and can say: "+studentGrade);
+//        if(Input==1){
+//            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Apple");
+//        }
+//        else if (Input==2){
+//            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Samsung");
+//        }
+//        else if (Input==3){
+//            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Xiomi");
+//        }
+//        else if (Input==4){
+//            System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: Google Pixel");
+//        }
+//        else System.out.println("Please choose option from 1-4");
     }
 }
