e93871ecad71d32a1f23a7e98f9d175432c2056c
Akmargam <aruway333@gmail.com>
2024-03-06 16:14:37 -0500

    Switch jdk 12< with input

--- src/Student.java
+++ src/Student.java
@@ -3,30 +3,20 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 public class Student {
     public static void main(String[]args) throws IOException {
-//        String stuName,stuAddress,stuGender;
-//        int stuId,stuAge;
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-
-        System.out.println("Enter Student name:");
-        String stuName= br.readLine();
-
-        System.out.println("Enter Student ID:");
-        int stuId= Integer.parseInt(br.readLine());
-
-        System.out.println("Enter Student age:");
-        int stuAge= Integer.parseInt(br.readLine());
-
-        System.out.println("Enter Student Address:");
-        String stuAddress= br.readLine();
-
-        System.out.println("Enter Student Gender:");
-        String stuGender= br.readLine();
-
-        System.out.println("Name of the student is: "+stuName);
-        System.out.println("ID of the student is: "+stuId);
-        System.out.println("Age of the student is: "+stuAge);
-        System.out.println("Address of the student is: "+stuAddress);
-        System.out.println("Gender of the student is: "+stuGender);
-
+        System.out.println("Please choose a number from 1 to 4: ");
+        int Input=Integer.parseInt(br.readLine());
+        String mobileName=switch (Input){
+            case 1->"Apple";
+            case 2->"Samsung";
+            case 3->{
+                System.out.println("There are a few Chinese mobile, however");
+                System.out.println("They are not good, nevertheless");
+                yield "Xiomi";
+            }
+            case 4->"Google Pixel";
+            default -> "Please choose option from 1-4";
+        };
+        System.out.println("Option you chose is: "+Input +"\t"+"Mobile is: "+mobileName);
     }
 }
