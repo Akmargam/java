7492a8c22b0b2b90d670742e6df9244245acaf20
Akmargam <aruway333@gmail.com>
2024-03-14 18:42:34 -0400

    class basics-vars in the class itself

--- src/ClassTwo.java
+++ src/ClassTwo.java
@@ -1,7 +1,7 @@
 
 class ClassTwoOps{
 
-    int var1,var2,result;
+    int var1=20,var2=20,result;
     void Multi(){
         result=var1*var2;
         System.out.println(result);
@@ -15,8 +15,8 @@ class ClassTwoOps{
 public class ClassTwo {
     public static void main(String[]args){
         ClassTwoOps obj1=new ClassTwoOps();
-        obj1.var1=20;
-        obj1.var2=10;
+//        obj1.var1=20;
+//        obj1.var2=10;
         obj1.Multi();
         obj1.Div();
     }
