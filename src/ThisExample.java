41cd3471d3b2db05ad0a370364c0a62d92b30676
Akmargam <aruway333@gmail.com>
2024-03-16 22:58:49 -0400

    this ex 3-illustrated

--- src/ThisExample.java
+++ src/ThisExample.java
@@ -1,14 +1,16 @@
 public class ThisExample {
     int a;
 
-   ThisExample(int a){
-       this.a=a;
-       get(this);
+    ThisExample(int a) {
+        this.a = a;
+        get(this); // Passes the current object to the 'get' method
     }
-    private void get(ThisExample obj){
-        System.out.println(obj.a);
+
+    private void get(ThisExample obj) {
+        System.out.println(obj.a); // Prints the value of 'a' from the passed object
     }
-    public static void main(String[]args){
-        ThisExample obj=new ThisExample(10);
+
+    public static void main(String[] args) {
+        ThisExample obj = new ThisExample(10); // Creates an instance of ThisExample
     }
 }
