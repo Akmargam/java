c2205ff3038f2460e956653f1ca1dd83dfe54790
Akmargam <aruway333@gmail.com>
2024-03-23 17:55:01 -0400

    Static polymorphism- compile time, method overloading

--- src/PolyDemo1.java
+++ src/PolyDemo1.java
@@ -1,8 +1,19 @@
 public class PolyDemo1 {
+
+    void sum(int i,int j){
+        System.out.println("Sum is:"+(i+j));
+    }
+    void sum(int i,int j,int k){
+        System.out.println("Sum is:"+(i+j+k));
+    }
+    void sum(int i,int j,int k, int l){
+        System.out.println("Sum is:"+(i+j+j+l));
+    }
   public static void main(String[]args){
-     int i=44;
-     double d=i;
-      System.out.println(d);
+     PolyDemo1 obj=new PolyDemo1();
+     obj.sum(1,2);
+     obj.sum(1,2,3,4);
+     obj.sum(1,2,3);
   }
 
 }
